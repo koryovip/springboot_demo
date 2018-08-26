171308700c827da3d5f490eb821eaaa3d035523d
koryo <koryovip@yahoo.co.jp>
2018-08-26 18:48:59 +0900

    add security

--- src/main/java/com/example/demo/b0000/B0000Controller.java
+++ src/main/java/com/example/demo/b0000/B0000Controller.java
@@ -1,12 +1,17 @@
 package com.example.demo.b0000;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class B0000Controller {
 
     @Autowired
     private B0000Service b0000Service;
 
+    @GetMapping(value = "/time", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String time() {
         b0000Service.select();
         return String.format("{%s}", json("time", System.currentTimeMillis()));
